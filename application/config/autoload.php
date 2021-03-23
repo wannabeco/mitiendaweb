1aca7afae255978e2fddb1c4e5980135c56d3a18
Farez Prieto <kyo20052@gmail.com>
2021-03-23 15:53:06 -0500

    Solucionando el bug de la clase Nexmo

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('nexmo');
+$autoload['libraries'] = array('Nexmo');
 
 /*
 | -------------------------------------------------------------------
