5b25e384064586d3649101365eefc636bd3c2328
Farez Prieto <kyo20052@gmail.com>
2021-06-11 15:01:52 -0500

    Ajuste en la creación del producto. Error con el count

--- application/models/home/LogicaHome.php
+++ application/models/home/LogicaHome.php
@@ -597,7 +597,7 @@ class LogicaHome  {
             $dataInsertar['idPresentacion']     = $post['idPresentacion'];
             $dataInsertar['idTienda']           = $post['idTienda'];
             $resultado = $this->ci->dbHome->agregaVariacion($dataInsertar);
-            if(count($resultado) > 0)
+            if($resultado)> 0)
             {
                 $salida = array("mensaje"=>"Categoría creada de manera exitosa",
                                 "datos"=>$resultado,
@@ -622,7 +622,7 @@ class LogicaHome  {
             $dataActualiza['idTienda']           = $post['idTienda'];
 
             $resultado = $this->ci->dbHome->actualizaVariacion($where,$dataActualiza);
-            if(count($resultado) > 0)
+            if($resultado > 0)
             {
                 $salida = array("mensaje"=>"Categoría actualizada de manera exitosa",
                                 "datos"=>$resultado,
@@ -672,7 +672,7 @@ class LogicaHome  {
         $where['idVariacion']           = $idVariacion;
         $where['idTienda']              = $idTienda;
         $resultado = $this->ci->dbHome->actualizaVariacion($where,$dataActualiza);
-        if(count($resultado) > 0)
+        if($resultado > 0)
         {
             //actualio todas las variaciones que tenga el producto
             $salida = array("mensaje"=>"La variacion se ha eliminado de manera corecta",
